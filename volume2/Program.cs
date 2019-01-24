d4c20061c1c49c84be79a7281a881e5414fd8363
cicley lee <cicley.lee@gmail.com>
2019-01-24 14:02:16 -0500

    Add project files.

---
+++ volume2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace volume
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            double volume, v;
+
+            Console.WriteLine("Enter the radius of the hemisphere:       ");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            volume = 4/3 * Math.PI * r * r * r;
+            v = volume/ Math.Sqrt(2);
+
+            Console.WriteLine("The volume is: "+ v);
+            Console.ReadLine();
+        }
+    }
+}
